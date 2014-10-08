05ecee34e4c985651924ae4bae378adfbd80c453
Keiran Raine <keiranmraine@gmail.com>
2014-10-08 15:21:35 +0100

    add useful readme and cleanup

--- prerelease.sh
+++ prerelease.sh
@@ -2,21 +2,21 @@
 
 ##########LICENCE##########
 # Copyright (c) 2014 Genome Research Ltd.
-# 
+#
 # Author: Cancer Genome Project cgpit@sanger.ac.uk
-# 
+#
 # This file is part of cgpCaVEManPostProcessing.
-# 
+#
 # cgpCaVEManPostProcessing is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Affero General Public License as published by the Free
 # Software Foundation; either version 3 of the License, or (at your option) any
 # later version.
-# 
+#
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 # FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 # details.
-# 
+#
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##########LICENCE##########
@@ -75,7 +75,7 @@ rm -f MANIFEST
 # cleanup things which could break the manifest
 rm -rf install_tmp
 perl Makefile.PL > /dev/null
-make manifest &> /dev/null
+make manifest >& /dev/null
 rm -f Makefile MANIFEST.bak pm_to_blib
 
 # change back to original dir
